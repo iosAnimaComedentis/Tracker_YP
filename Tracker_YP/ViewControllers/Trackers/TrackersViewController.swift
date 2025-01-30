6b46a6db03827f329e0a91c4177f000d57ea76ca
Максим Карпов <maximmangk.petrov@yandex.ru>
2025-01-30 18:34:27 +0300

    add tabbar

--- Tracker_YP/ViewControllers/Trackers/TrackersViewController.swift
+++ Tracker_YP/ViewControllers/Trackers/TrackersViewController.swift
@@ -7,8 +7,58 @@
 import UIKit
 
 final class TrackersViewController: UIViewController {
+    //MARK: - Private Properties
+    private var currentDate: Date = Date()
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.calendar.firstWeekday = 2
+        return datePicker
+    }()
+    
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .colorSelected10
+        view.backgroundColor = .ypWhite
+        
+        navigationBar()
+    }
+    
+    //MARK: - Private Methods
+    private func navigationBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "",
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        navigationItem.leftBarButtonItem?.image = UIImage(systemName: "plus")
+        navigationItem.leftBarButtonItem?.target = self
+        navigationItem.leftBarButtonItem?.action = #selector(addTarget)
+        navigationItem.leftBarButtonItem?.tintColor = .ypBlack
+        
+        let datePickerBarButtonItem = UIBarButtonItem(customView: datePicker)
+        datePickerBarButtonItem.customView?.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        navigationItem.rightBarButtonItem = datePickerBarButtonItem
+        
+        datePicker.addTarget(self, action: #selector(datePickerDateSelection), for: .valueChanged)
+        
+        navigationItem.title = "Трекеры"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+    }
+    
+    @objc
+    private func addTarget(){
+        
+    }
+    
+    @objc private func datePickerDateSelection() {
+        
     }
 }
